52f263fd5363e52d63f78a2179288aabd7b7413e
Paul Savignano <Paul.Savignano@gmail.com>
2016-08-15 16:06:20 -0700

    Added Swarm component, route, and navigation.

---
+++ imports/ui/pages/swarm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Bert } from 'meteor/themeteorchef:bert'
+import { insertDocument } from '../../api/documents/methods.js'
+
+const callMethod = (index) => {
+  insertDocument.call({ title: `Document #${index + 1}` }, (error) => {
+    if (error) Bert.alert(error.reason, 'danger')
+  })
+}
+
+const swarm = () => {
+  for (let i = 0; i < 25; i++) {
+    callMethod(i)
+  }
+}
+
+export const Swarm = () => (
+  <Button onClick={ swarm } bsStyle="success">
+    Call insertDocument
+  </Button>
+)
